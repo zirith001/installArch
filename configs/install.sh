99827f1ee649771756440c4d4719ce84da4e97b0
zirith001 <141006730+zirith001@users.noreply.github.com>
2023-08-02 05:23:38 +0530

    Refactored and added some changes

--- configs/install.sh
+++
@@ -1,7 +0,0 @@
-cd
-cp ./configs/10-monitor.conf /etc/X11/xorg.conf.d/10-monitor.conf
-cp ./configs/40-libinput.conf /etc/X11/xorg.conf.d/40-libinput.conf
-cp ./configs/.Xresources ~/.Xresources
-cp ./configs/.xinitrc ~/.xinitc
-echo "startx" >> ~/.bash_profile
-sudo shutdown
