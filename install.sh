96a1fe46907cf57a1447b723a896a214e19b292f
zirith001 <141006730+zirith001@users.noreply.github.com>
2023-08-02 05:58:52 +0530

    Fixed filename in install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-sudo pacman -S spice-vdagent xf86-video-qxl git vim xorg-server xorg-apps xorg-xinit xclip xdotool i3 noto-fonts noto-fonts-emoji ttf-firacode-nerd rxvt-unicode alacritty kitty dmenu rofi firefox
+sudo pacman -S spice-vdagent xf86-video-qxl vim xorg-server xorg-apps xorg-xinit xclip xdotool i3 noto-fonts noto-fonts-emoji ttf-firacode-nerd rxvt-unicode alacritty kitty dmenu rofi firefox
 
 CURRENT_DIR=$(pwd)
 cp $CURRENT_DIR/configs/10-monitor.conf /etc/X11/xorg.conf.d/10-monitor.conf
 cp $CURRENT_DIR/configs/40-libinput.conf /etc/X11/xorg.conf.d/40-libinput.conf
 cp $CURRENT_DIR/configs/.Xresources ~/.Xresources
-cp $CURRENT_DIR/configs/.xinitrc ~/.xinitc
+cp $CURRENT_DIR/configs/.xinitrc ~/.xinitrc
 echo "startx" >> ~/.bash_profile
 sudo reboot
